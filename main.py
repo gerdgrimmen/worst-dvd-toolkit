23e46f349c21f87007ef5798e47c538acbcf18fe
Gerd Grimmen <gerd.grimmen.gg@gmail.com>
2025-11-12 11:26:22 +0100

    feature: 0.1.0

--- main.py
+++ main.py
@@ -1,12 +1,66 @@
 import subprocess
-lsdvd = subprocess.run(["lsdvd", "/dev/sr0"], capture_output=True, text=True)
+import sys
+from shutil import which
 
-print(lsdvd.stdout)
+version = "0.1.0"
 
-# simple info?
-#$ dvdbackup -i /dev/sr0 -I
-# full dvd
-#$ dvdbackup -i /dev/dvd -M -p
-dvdbackup = subprocess.run(["dvdbackup","-i", "/dev/sr0", "-M", "-p"], capture_output=True, text=True)
+def arguments_guard():
+    if len(sys.argv) < 2:
+        print("There is no command given. Nothing To Do here:")
+        command_help()
+        exit()
 
-print(dvdbackup.stdout)
+def command_guard(command, length):
+    print("checking structure for: " + command)
+    if len(sys.argv) == length:
+        return True
+    else:
+        print("not the right amount of arguments.")
+        exit()
+
+def check_arguments_structure(command):
+    match command:
+        case "info":
+            if command_guard("info", 3):
+                lsdvd = subprocess.run(["lsdvd", sys.argv[2]], capture_output=True, text=True)
+                print(lsdvd.stdout)
+
+def command_version():
+    print(version)
+
+def command_help():
+    print("Version:")
+    command_version()
+    print("Help Page")
+    print("Type '<script_name> help' to get a list of commands")
+    print("")
+    print("Commands:")
+    print("info <path_to_dvd>")
+    print("uses the programm lsdvd")
+
+def command_check_dependencies():
+    print("You do not need every dependency to run the toolkit: ")
+    if which("lsdvd"):
+        print("lsdvd is found. Available commands are:")
+        print("info")
+    if which("dvdbackup"):
+        print("dvdbackup is found. Available commands are:")
+        print("backup (not yet implemented)")
+    if which("ffmpeg"):
+        print("ffmpeg is found. Available commands are:")
+        print("convert (not yet implemented)")
+
+def determine_command(command):
+    match command:
+        case "help":
+            command_help()
+        case "version":
+            command_version()
+        case "check":
+            command_check_dependencies()
+        case "info":
+            check_arguments_structure("info")
+
+if __name__ == "__main__":
+    arguments_guard()
+    determine_command(sys.argv[1])
